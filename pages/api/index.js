088cc357ef999d1875174d40a07117dcb64cf933
Jeiel Lima Miranda <github.esvlc@slmail.me>
2024-09-20 15:04:53 +0000

    fix

--- pages/api/index.js
+++ pages/api/index.js
@@ -5,8 +5,26 @@ export default async function handler(req, res) {
   const { name, Telefone, email, cpf, random } = query;
 
   const domain = 'https://my-pix.vercel.app';
-  const staticUrl = `${domain}/view?name=${encodeURIComponent(name)}&Telefone=${encodeURIComponent(Telefone)}&email=${encodeURIComponent(email)}&cpf=${encodeURIComponent(cpf)}&random=${encodeURIComponent(random)}`;
 
+  // Criar um objeto com os parâmetros opcionais
+  const params = {
+    name,
+    Telefone,
+    email,
+    cpf,
+    random
+  };
+
+  // Filtrar parâmetros não fornecidos (undefined ou null) e gerar a query string dinamicamente
+  const filteredParams = Object.entries(params)
+    .filter(([key, value]) => value !== undefined && value !== null)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
+
+  // Gera a URL completa para a página estática
+  const staticUrl = `${domain}/view?${filteredParams}`;
+
+  // Faz uma solicitação ao is.gd para encurtar a URL gerada
   try {
     const response = await fetch(`https://is.gd/create.php?format=simple&url=${encodeURIComponent(staticUrl)}`);
     const shortUrl = await response.text();

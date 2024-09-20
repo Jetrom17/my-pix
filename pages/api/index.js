1f2be81f4761b15c2c509db030a683253db80236
Jeiel Lima Miranda <github.esvlc@slmail.me>
2024-09-20 15:00:20 +0000

    up

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,20 +4,15 @@ export default async function handler(req, res) {
   const { query } = req;
   const { name, Telefone, email, cpf, random } = query;
 
-  // O domínio deve ser inserido manualmente ou obtido dinamicamente
   const domain = 'https://my-pix.vercel.app';
-
-  // Gera a URL completa para a página estática
   const staticUrl = `${domain}/view?name=${encodeURIComponent(name)}&Telefone=${encodeURIComponent(Telefone)}&email=${encodeURIComponent(email)}&cpf=${encodeURIComponent(cpf)}&random=${encodeURIComponent(random)}`;
 
-  // Faz uma solicitação ao is.gd para encurtar a URL gerada
   try {
     const response = await fetch(`https://is.gd/create.php?format=simple&url=${encodeURIComponent(staticUrl)}`);
     const shortUrl = await response.text();
 
-    // Redireciona o usuário diretamente para a URL encurtada
-    res.writeHead(302, { Location: shortUrl });
-    res.end();
+    // Em vez de redirecionar, devolva a URL encurtada como resposta
+    res.status(200).json({ shortUrl });
   } catch (error) {
     res.status(500).json({ error: 'Erro ao encurtar a URL' });
   }
